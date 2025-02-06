eed6cd275eb32321465b26f0fb4b3b6af6831e1f
dima tkachenko <dtkachenko06@gmail.com>
2025-02-06 16:51:58 +0200

    make register page

---
+++ src/Slices/serverSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const serverSlice = createSlice({
+    name: "Server",
+    initialState: {
+        serverURL:"localhost:3210"
+    },
+    reducers:{}
+})
+
+export default serverSlice.reducer
