26d0315a6c4f2482ed78740d686f785220dc27c4
Abobiusus <axe.kirito@gmail.com>
2025-02-13 16:53:33 +0200

    +Player (Post Get Patch Delete)

--- server/src/player/player.controller.ts
+++ server/src/player/player.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PlayerService } from './player.service';
+import { CreatePlayerDto, UpdatePlayerDto } from './player.dto';
 
 @Controller('player')
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+    create(@Body() dto: CreatePlayerDto) {
+      return this.playerService.createPlayer(dto);
+    }
+
+    @Get(':userid')
+    async getPlayerByUserId(@Param('userid') userid: number) {
+      return this.playerService.getPlayerByUserId(userid);
+    }
+  
+    @Patch(':userid')
+    async updatePlayer(@Param('userid') userid: number, @Body() data: UpdatePlayerDto) {
+      return this.playerService.updatePlayer(userid, data);
+    }
+  
+    @Delete(':userid')
+    async deletePlayer(@Param('userid') userid: number) {
+      return this.playerService.deletePlayer(userid);
+    }
+
 }
--- server/src/player/player.service.ts
+++ server/src/player/player.service.ts
@@ -1,7 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CreatePlayerDto, UpdatePlayerDto } from './player.dto';
+
 
 @Injectable()
 export class PlayerService {
     constructor(private readonly prisma:PrismaService){}
+    async createPlayer(data: CreatePlayerDto) {
+        return this.prisma.players.create({
+          data: {
+            userid: data.userid,
+            cards: data.cards,
+            roomid: data.roomid,
+            status: data.status,
+          },
+        });
+      }
+
+      async getPlayerByUserId(userid: number) {
+        const player = await this.prisma.players.findUnique({
+          where: { userid },
+        });
+        if (!player) {
+          throw new NotFoundException('Player not found');
+        }
+        return player;
+      }
+    
+      async updatePlayer(userid: number, data: UpdatePlayerDto) {
+        return this.prisma.players.update({
+          where: { userid },
+          data,
+        });
+      }
+    
+      async deletePlayer(userid: number) {
+        return this.prisma.players.delete({
+          where: { userid },
+        });
+      }
 }
