508eba2c0c431dbb37184eaa8a76c5151d02d6f2
dima tkachenko <dtkachenko06@gmail.com>
2025-02-13 16:43:19 +0200

    add create Room

--- src/Pages/LobbyPage/index.tsx
+++ src/Pages/LobbyPage/index.tsx
@@ -1,42 +1,32 @@
 import axios from "axios";
 import Room from "./Room";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Lobbys() {
+    const navigate = useNavigate();
     useEffect(() => {
         const getRooms = async() =>{
             const rooms = await axios({
                 method: "get",
                 url: `http://localhost:3210/api/room`
             })
+            console.log(rooms)
         }
+        getRooms()
     })
-    const data =  [
-        {
-            id : 1,
-            name : "name",
-            status : "online",
-            usersid : ["1","2","3"],
-        },
-        {
-            id : 2,
-            name : "name2",
-            status : "online",
-            usersid : ["4","5","6"],
-        },
-
-    ]
     const somefunc = ()=>{
         console.log("ABOBA")
     }
-
+    const data = []
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen w-full overflow-y-auto p-5 lobby">
             {data.map((i) => (
                 <Room key={i.id} data={i} connectionFunction={somefunc} />
             ))}
+            <button className="createRoom" onClick ={() => {navigate("/CreateRoom")}}>Create Room</button>
         </div>
     );
 }
--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -3,3 +3,4 @@ export {default as UserPage} from "./UserPage/UserPage"
 export {default as RegisterPage} from "./RegisterPage/Register"
 export {default as LoginPage} from "./LoginPage/Login"
 export {default as LobbyPage} from "./LobbyPage"
+export {default as CreateRoomPage} from "./CreateRoom/createRoom";
