7b9970b6ee12a1473a942cce189f3a14cf8bd24c
Abobiusus <axe.kirito@gmail.com>
2025-06-14 17:25:49 +0300

    fix1000%

--- server/src/rooms/rooms.module.ts
+++ server/src/rooms/rooms.module.ts
@@ -4,9 +4,12 @@ import { RoomsController } from './rooms.controller';
 import { PrismaService } from '../prisma.service';
 import { RoomsGateway } from './rooms.gateway';
 import { PlayerModule } from 'src/player/player.module';
+import { PockerModule } from 'src/pocker/pocker.module';
+import { StepModule } from 'src/step/step.module';
+import { UserModule } from 'src/User/user.module';
 
 @Module({
-  imports:[PlayerModule],
+  imports: [PrismaService, PlayerModule, PockerModule, StepModule, UserModule],
   controllers: [RoomsController],
   providers: [RoomsService, PrismaService, RoomsGateway],
 })
