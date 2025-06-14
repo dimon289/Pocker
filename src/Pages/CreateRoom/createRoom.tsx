4c1656d2679dc992154fc3775f59ed8442761181
dimon289 <65030233+dimon289@users.noreply.github.com>
2025-06-14 19:37:52 +0300

    fix1

--- src/Pages/CreateRoom/createRoom.tsx
+++ src/Pages/CreateRoom/createRoom.tsx
@@ -33,7 +33,7 @@ function CreateRoom() {
                 url:`${apiUrl}/api/rooms/create`,
                 data:{
                     name:  roomName,
-                    usersid: userid,
+                    userID: [userid],
                     password : roomPassword,
                 }
             })
