a442e0f3693853c6575afcb82ecde04b5ec1a66d
dimon289 <65030233+dimon289@users.noreply.github.com>
2025-06-18 19:15:58 +0300

    pull

--- src/Pages/Homepage/homepage.tsx
+++ src/Pages/Homepage/homepage.tsx
@@ -66,9 +66,7 @@ function Home() {
                     <div className="user">
                         <Link to="User">{name}</Link>
                     </div>
-                    <div className="balance" onClick={async()=>{
-                        
-                    }}>
+                    <div className="balance">
                         {(balance * 100).toLocaleString("en-US", { style: "currency", currency: "USD" })}
                     </div>
                 </div>
