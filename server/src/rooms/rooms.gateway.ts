44b3f1343fe77b185b86ee8350328f2857470a90
dimon289 <65030233+dimon289@users.noreply.github.com>
2025-06-19 13:04:04 +0300

    1.02

--- server/src/rooms/rooms.gateway.ts
+++ server/src/rooms/rooms.gateway.ts
@@ -12,7 +12,7 @@ import { error, table } from 'console';
 import { first } from 'rxjs';
 import e from 'express';
 
-@WebSocketGateway({ namespace: '/rooms', cors: { origin: 'http://localhost:5173', credentials: true } })
+@WebSocketGateway({ namespace: '/rooms', cors: { origin: 'http://142.93.175.150', credentials: true } })
 @Injectable()
 export class RoomsGateway implements OnGatewayConnection {
   @WebSocketServer()
